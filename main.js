21f87936bf2b6ec4708a56042d14c7e713720673
christopherwperry <christopherwperry@gmail.com>
2017-08-07 20:50:32 -0400

    onclick event and function given to numbers and display div

--- main.js
+++ main.js
@@ -1,13 +1,84 @@
 //eval() to resolve
 
-let operators = document.getElementsByClassName("operator");
+let operators = document.querySelectorAll(".operator");
+let display = document.getElementById("display");
+let numbers = document.querySelectorAll(".number");
+let output;
+let zero;
+let decimal;
 
-console.log(operators);
+for (let i = 0; i<numbers.length; i++){
+  numbers[i].addEventListener("click", function() {
+    output = display.innerHTML +=this.value;
+    }, false)
+  };
 
-for (let i = 0; i<operators.length; i++){
-  operators.addEventListener("click", logValue, true);
+document.querySelector(".zero").addEventListener("click",function() {
+  zero = this.value;
+  if(display.innerHTML === "") {
+    output = display.innerHTML = zero;
+    } else if(display.innerHTML === output) {
+         output = display.innerHTML +=zero;
+        }
+    },false);
+
+document.getElementById("decimal").addEventListener("click",function() {
+  period = this.value;
+  if(display.innerHTML === "") {
+    output = display.innerHTML = display.innerHTML.concat("0.");
+    } else if(display.innerHTML === output) {
+        display.innerHTML = display.innerHTML.concat(".");
+        }
+  },false);
+
+
+    /*
+    document.querySelector("#eqn-bg").addEventListener("click",function() {
+
+      if(display.innerHTML === output) {
+
+        display.innerHTML = eval(output);
+      }
+
+      else {
+            display.innerHTML = "";
+      }
+
+    },false);
+
+ document.querySelector("#delete").addEventListener("click",function() {
+
+        display.innerHTML = "";
+
+    },false);
+
+
+     var elem1 = document.querySelectorAll(".operator");
+
+      var len1 = elem1.length;
+
+      for(var i = 0; i < len1; i++ ) {
+
+        elem1[i].addEventListener("click",function() {
+
+        operator = this.value;
+
+         if(display.innerHTML === "") {
+
+            display.innerHTML = display.innerHTML.concat("");
+
+        }
+
+        else if(output) {
+
+            display.innerHTML = output.concat(operator);
+
+        }
+
+    },false);
+
+
+
+      }
 }
-
-function logValue() {
-
-};
+    */
